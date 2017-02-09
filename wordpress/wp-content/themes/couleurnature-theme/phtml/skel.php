a9945c7b35dcac4e64a35bc93366690af3062821
thomasbatt <thomasbatt@gmail.com>
2017-02-09 09:50:59 +0100

    init structure

--- wordpress/wp-content/themes/couleurnature-theme/phtml/skel.php
+++ wordpress/wp-content/themes/couleurnature-theme/phtml/skel.php
@@ -371,8 +371,8 @@ html {
 </style>
 
 		
-		<link rel='stylesheet' href='<?php bloginfo('template_url')?>/css/vendors.min.css?v=317c82608f8'/>
-		<link rel='stylesheet' href='<?php bloginfo('template_url')?>/css/website.min.css?v=317c82608f8'/>
+		<link rel='stylesheet' href='<?php bloginfo('template_url')?>/css/vendors.min.css?v=317e8943470'/>
+		<link rel='stylesheet' href='<?php bloginfo('template_url')?>/css/website.min.css?v=317e8943470'/>
 	</head>
 
 	<body>
@@ -384,6 +384,6 @@ html {
 	</body>
 
 	
-	<script type='text/javascript' src='<?php bloginfo('template_url');?>/js/vendors.min.js?v=317c82608f8'></script>
-	<script type='text/javascript' src='<?php bloginfo('template_url');?>/js/website.min.js?v=317c82608f8'></script>
+	<script type='text/javascript' src='<?php bloginfo('template_url');?>/js/vendors.min.js?v=317e8943470'></script>
+	<script type='text/javascript' src='<?php bloginfo('template_url');?>/js/website.min.js?v=317e8943470'></script>
 </html>
